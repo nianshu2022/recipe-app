5444a083c1dc38dd4422be3a17a6f4cc13bbdaaa
NianShu <nianshu@example.com>
2026-06-19 18:53:06 +0800

    release: v1.1.4

--- android/app/src/main/java/com/nianshu/recipeapp/MainActivity.java
+++ android/app/src/main/java/com/nianshu/recipeapp/MainActivity.java
@@ -1,5 +1,18 @@
 package com.nianshu.recipeapp;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // 禁止全屏模式，确保内容在状态栏下方
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        // 设置状态栏颜色
+        getWindow().setStatusBarColor(0xFFA8432D); // colorPrimaryDark
+    }
+}
